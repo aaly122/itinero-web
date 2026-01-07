94edd47dcb31a7f51991085b94c597eb36f04a5f
karlo-alano <alano.juankarlo@gmail.com>
2026-01-07 23:20:56 +0800

    feat(backend): added fallback to create page if formstore is empty

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,16 +1,26 @@
 <script setup>
 import Map from '@/components/Map.vue';
 import { useFormStore } from '@/store/formStore.js';
+import { useRouter } from 'vue-router';
 
+import { onBeforeMount } from 'vue';
 
+const router = useRouter();
 const formStore = useFormStore();
+onBeforeMount(() => {
 
-console.log("Form store:", formStore.tripData.stops)
+        if (!formStore.tripData || !formStore.tripData.stops) {
+            router.push('/Create'); 
+        }
+    });
 </script>
 
 <template>
-  <div class="overflow-hidden">
+  <div v-if="formStore.tripData && formStore.tripData.stops" class="overflow-hidden">
    <Map /> 
+  </div>
+  <div v-else class="flex h-screen w-screen items-center justify-center">
+        <p>Redirecting...</p>
   </div>
 
 </template>
